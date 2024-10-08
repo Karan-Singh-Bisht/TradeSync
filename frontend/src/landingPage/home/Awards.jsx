40a7ba9ae469f57201bf432c2cc06d2c07c5b1aa
Karan <karansbisht7@gmail.com>
2024-10-08 15:25:28 +0530

    Created all components except navbar and footer

--- frontend/src/landingPage/home/Awards.jsx
+++ frontend/src/landingPage/home/Awards.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 function Awards() {
-  return <div>Awards</div>;
+  return (
+    <div className="container mt-4 p-5" style={{ marginBottom: "3vw" }}>
+      <div className="row">
+        <div className="col-lg-6 col-sm-12">
+          <img
+            className="img-fluid"
+            src="Images/largestBroker.svg"
+            alt="Award SVG"
+          />
+        </div>
+        <div className="col-lg-6 col-sm-12 mt-3">
+          <h1 className="mb-3">Largest Stock Broker in India</h1>
+          <p>
+            2+ million Zerodha clients contribute to over 15% of all retail
+            order <br />
+            volumes in India daily by trading and investing in:
+          </p>
+          <div className="row">
+            <div className="col-6 mt-4">
+              <ul>
+                <li className="mb-3">Futures and Options</li>
+                <li className="mb-3">Commodity Derivatives</li>
+                <li className="mb-3">Currency Derivatives</li>
+              </ul>
+            </div>
+            <div className="col-6 mt-4 mb-3">
+              <ul>
+                <li className="mb-3">Stocks and IPOs</li>
+                <li className="mb-3">Direct Mutual Funds</li>
+                <li className="mb-3">Bonds and Govt. Securities</li>
+              </ul>
+            </div>
+          </div>
+          <img
+            className="img-fluid"
+            style={{ width: "90%" }}
+            src="Images/pressLogos.png"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- frontend/src/landingPage/home/Stats.jsx
+++ frontend/src/landingPage/home/Stats.jsx
@@ -1,7 +1,71 @@
 import React from "react";
 
 function Stats() {
-  return <div>Stats</div>;
+  return (
+    <div className="container p-3">
+      <div className="row p-5">
+        <div className="col-md-12 col-lg-6">
+          <h1 className="my-5">
+            <strong>Trust with Confidence</strong>
+          </h1>
+          <h3 className="mt-4">Customer-first Always</h3>
+          <p className="text-muted mb-4">
+            That's why 1.5+ crore customers trust Zerodha with <br />
+            ₹4.5+ lakh crores of equity investments and contribute to <br />
+            15% of daily retail exchange volumes in India.
+          </p>
+          <h3>No spam or gimmicks</h3>
+          <p className="text-muted mb-4">
+            No gimmicks, spam, "gamification", or annoying push
+            <br />
+            notifications. High quality apps that you use at your
+            <br /> pace, the way you like.
+          </p>
+          <h3>The Zerodha Universe</h3>
+          <p className="text-muted mb-4">
+            Not just an app, but a whole ecosystem. Our investments <br />
+            in 30+ fintech startups offer you tailored services
+            <br /> specific to your needs.
+          </p>
+          <h3>Do better with money</h3>
+          <p className="text-muted mb-4">
+            With initiatives like{" "}
+            <a style={{ textDecoration: "none" }} href="#">
+              Nudge
+            </a>{" "}
+            and{" "}
+            <a style={{ textDecoration: "none" }} href="#">
+              Kill Switch
+            </a>
+            , we don't just
+            <br />
+            facilitate transactions, but actively help you do better
+            <br /> with your money.
+          </p>
+        </div>
+        <div className="col-md-12 col-lg-6">
+          <img
+            className="img-fluid mt-5"
+            src="Images/ecosystem.png"
+            alt="Technology Image"
+          />
+          <div className="row text-center mt-3">
+            <div className="col">
+              <a
+                style={{ textDecoration: "none", marginRight: "2vw" }}
+                href="#"
+              >
+                Explore Our Products <i className="fa-solid fa-arrow-right"></i>
+              </a>
+              <a style={{ textDecoration: "none" }} href="#">
+                Try Kite Demo <i className="fa-solid fa-arrow-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Stats;
