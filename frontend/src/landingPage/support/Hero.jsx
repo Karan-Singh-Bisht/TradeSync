b642a9a56f6453525336f4f856d2e5b82605831a
Karan <karansbisht7@gmail.com>
2024-10-10 23:40:30 +0530

    landing page completed

--- frontend/src/landingPage/support/Hero.jsx
+++ frontend/src/landingPage/support/Hero.jsx
@@ -1,7 +1,52 @@
 import React from "react";
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="container-fluid" id="supportHero">
+      <div className="p-5 text-center" id="supportWrapper">
+        <h4>Support Portal</h4>
+        <a href="">Track Tickets</a>
+      </div>
+
+      <div className="row p-5 m-3">
+        {/* Left Column */}
+        <div className="col-lg-6 col-md-12 p-3">
+          <h1 className="fs-3">
+            Search for an answer or browse help topics to create a ticket
+          </h1>
+          <input
+            className="form-control my-3"
+            placeholder="Eg. how do I activate F&O"
+          />
+          <a href="" className="d-block mb-2">
+            Track account opening
+          </a>
+          <a href="" className="d-block mb-2">
+            Track segment activation
+          </a>
+          <a href="" className="d-block mb-2">
+            Intraday margins
+          </a>
+          <a href="" className="d-block mb-2">
+            Kite user manual
+          </a>
+        </div>
+
+        {/* Right Column */}
+        <div className="col-lg-6 col-md-12 p-3">
+          <h1 className="fs-3">Featured</h1>
+          <ol>
+            <li>
+              <a href="">Current Takeovers and Delisting - January 2024</a>
+            </li>
+            <li>
+              <a href="">Latest Intraday leverages - MIS & CO</a>
+            </li>
+          </ol>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
